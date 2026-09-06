5a66cebce819cf10ca7743dce71317d42c0cb67d
Sean Collins <scollinspt@tutanota.com>
2026-09-06 05:33:57 -0400

    Initialize Pygame game loop

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+from logger import log_state
+
+pygame.init()
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
