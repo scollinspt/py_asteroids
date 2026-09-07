194fde9d5ef4b26ca0c69c231a501c2173fe6955
Sean Collins <scollinspt@tutanota.com>
2026-09-07 06:46:07 -0400

    Build player and asteroid gameplay

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import sys
+
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
-from logger import log_state
+from logger import log_event, log_state
+from player import Player
 
 pygame.init()
 clock = pygame.time.Clock()
-dt = 0.0
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    dt = 0.0
 
     while True:
         log_state()
@@ -17,7 +30,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0   
 
